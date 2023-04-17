5d6ad205b7aaf37c4ca8bf34647d174db2d5aa5e
oluwayemisi oguntayo <olashiku@gmail.com>
2023-04-17 09:11:51 +0100

    my utility folder

---
+++ DeepLinkUtils.kt
@@ -0,0 +1,59 @@
+package com.Corestep.androidapp.utility
+
+import android.app.Activity
+import android.net.Uri
+import android.util.Log
+import com.google.firebase.dynamiclinks.DynamicLink
+
+import com.google.firebase.dynamiclinks.ktx.androidParameters
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.ktx.iosParameters
+import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.dynamiclinks.ktx.component1
+import com.google.firebase.dynamiclinks.ktx.component2
+
+object DeepLinkUtils {
+
+  private  const val coreStepWebsiteUrl = "https://www.corebanknigeria.com/"
+  private  const val coreStepDeeplinkUrl = "https://corestep.page.link"
+  private  const val coreStepAndroidDomainName = "com.Corestep.androidapp"
+  private  const val coreStepIosDomainName = "com.Corestep.iosapp"
+
+
+    fun generateDeepLink(customerId: String, action: (String?) -> Unit){
+    val dynamicLink =    Firebase.dynamicLinks.createDynamicLink()
+            .setLink(Uri.parse(coreStepWebsiteUrl+customerId))
+            .setDomainUriPrefix(coreStepDeeplinkUrl)
+            .setAndroidParameters(DynamicLink.AndroidParameters.Builder().build())
+            .setIosParameters(DynamicLink.IosParameters.Builder(coreStepIosDomainName).build())
+            .buildDynamicLink()
+
+       Firebase.dynamicLinks.createDynamicLink()
+            .setLongLink(dynamicLink.uri)
+            .buildShortDynamicLink()
+            .addOnCompleteListener {
+                action.invoke(it.result?.shortLink.toString())
+            }
+            .addOnFailureListener {
+                println(it)
+                it.localizedMessage?.let { it1 -> Log.e("DEEPLINKUTILS", it1) }
+            }
+    }
+
+
+    fun getReferralCode(activity: Activity, action:(String?)->Unit) {
+        Firebase.dynamicLinks.getDynamicLink(activity.intent)
+            .addOnSuccessListener { pendingDynamicLinkData ->
+                if (pendingDynamicLinkData != null){
+                    val  deepLink = pendingDynamicLinkData.link
+                    val mystring = deepLink.toString().substring(deepLink.toString().indexOf("m/") + 2, deepLink.toString().length)
+                    action.invoke(mystring)
+                } else {
+                    action.invoke(null)
+                }
+            }.addOnFailureListener {
+                action.invoke(null)
+            }
+    }
+}
